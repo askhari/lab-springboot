113b2332c8376c4f10329f305fe2bce5536b115f
David Vidal Villamide Ext <dvidal.ext@hotelbeds.com>
2019-02-06 11:45:25 +0100

    Initial commit

---
+++ src/main/java/com/hotelbeds/springbootlab1/SpringbootLab1Application.java
@@ -0,0 +1,14 @@
+package com.hotelbeds.springbootlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLab1Application.class, args);
+	}
+
+}
+
